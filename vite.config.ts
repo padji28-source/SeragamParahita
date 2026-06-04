e6fa2223cfb2c6d00c41e7fb4e309252c51f4cc8
padji28-source <padji28@gmail.com>
2026-06-04 11:51:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,38 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(), 
+      tailwindcss(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'Parahita Garment',
+          short_name: 'Parahita',
+          description: 'Aplikasi Parahita Garment',
+          theme_color: '#ffffff',
+          background_color: '#ffffff',
+          display: 'standalone',
+          icons: [
+            {
+              src: '/Parahitaprimasentosa.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: '/Parahitaprimasentosa.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      })
+    ],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
